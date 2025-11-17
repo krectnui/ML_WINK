7bd0fc917e64011cd782ea914ad74777ac3e8445
Sombion <sombion@mail.ru>
2025-11-17 16:35:33 +0300

    Добавление CORS

--- app/main.py
+++ app/main.py
@@ -7,3 +7,27 @@ app = FastAPI()
 
 
 app.include_router(llm_router)
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5501",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
+)
